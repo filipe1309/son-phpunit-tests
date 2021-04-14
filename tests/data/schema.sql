4ac86aedfd762bb45634ec008c59adb290841893
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-04-14 13:28:15 -0300

    tests: add db files

---
+++ tests/data/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE products (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR (255) NOT NULL,
+    price FLOAT NOT NULL,
+    quantity INT NOT NULL,
+    total FLOAT NOT NULL
+);
